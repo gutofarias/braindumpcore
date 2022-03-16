598ee112c74498eaecd3470ca2c10c3a24acb1af
Joao Gutemberg Farias <joao.gutemberg.farias@gmail.com>
2022-03-16 12:49:06 -0300

    Apagando os arquivos .org.. Tinha pegado os errados

--- org/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((org-mode . ((org-hugo-base-dir . "..")
-              (eval . (org-hugo-auto-export-mode)))))
