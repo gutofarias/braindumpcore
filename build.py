acfecffa9151f34fb517221ca6e676f4a4849ac1
Joao Gutemberg Farias <joao.gutemberg.farias@gmail.com>
2022-03-16 12:05:31 -0300

    Modifiquei os arquivos iniciais pra adaptar ao meu problema

--- build.py
+++ build.py
@@ -4,17 +4,18 @@
 import glob
 from pathlib import Path
 
-files = glob.glob("org/*.org")
+files = glob.glob('/Users/guto/Sync/Projetos/org/hugo/org/**/*.org')
 
 with open('build.ninja', 'w') as ninja_file:
     ninja_file.write("""
 rule org2md
-  command = emacs --batch -l ~/.emacs.d/init.el -l publish.el --eval \"(jethro/publish \\"$in\\")"
+  command = emacs --batch -l ~/.emacs -l /Users/guto/Sync/Projetos/org/hugo/publish.el --eval \"(jethro/publish \\"$in\\")\"
   description = org2md $in
 """)
     
     for f in files:
         path = Path(f)
+        print(f)
         output_file = f"content/posts/{path.with_suffix('.md').name}"
         ninja_file.write(f"""
 build {output_file}: org2md {path}
